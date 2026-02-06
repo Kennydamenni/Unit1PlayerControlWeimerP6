a460479f8a1a757746cdef8f71b5da4b91467a7c
Kennydamenni <736659@student.dusd.net>
2026-02-06 13:43:19 -0800

    di the npc
    yeyeyyeyeyeyeyeyyeyeyeyeyeyeyeyeyeyey

--- P6PlayerControlWeimer/Assets/TutorialInfo/Scripts/PlayerController.cs
+++ P6PlayerControlWeimer/Assets/TutorialInfo/Scripts/PlayerController.cs
@@ -4,26 +4,26 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 25.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // This is wheere we get player input
         horizontalInput = Input.GetAxis("Horizontal");
-        fowardInput = Input.GetAxis("Vertical");
-        
-        // We'll move the vehicle forward
-       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       transform.Translate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward basedf on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates thhe car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
     }
 }
